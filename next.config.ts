2364f5ef4da21f348dc06c3fbae2e1f16b867cd5
Suchit G <suchitg04@gmail.com>
2025-06-02 20:46:24 +0530

    azure deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: 'export',  // Enables static exports
-  basePath: isProd ? '/todo_app' : '',
-  assetPrefix: isProd ? '/todo_app/' : '',
   images: { unoptimized: true }, // if you use next/image
 };
 
